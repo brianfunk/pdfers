9b8bcb40c9d0c14b54c6631d5b1b725d35a61eda
Brian Funk <brianfunk@users.noreply.github.com>
2023-03-23 00:05:35 -0400

    Update HtmlToPdfController.java

--- src/main/java/com/mycompany/myapp/web/rest/HtmlToPdfController.java
+++ src/main/java/com/mycompany/myapp/web/rest/HtmlToPdfController.java
@@ -2,6 +2,7 @@ package com.mycompany.myapp.web.rest;
 
 import com.mycompany.myapp.service.HtmlToPdfService;
 import java.io.IOException;
+import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,25 +23,27 @@ public class HtmlToPdfController {
         this.htmlToPdfService = htmlToPdfService;
     }
 
-    @PostMapping(value = "/generate-pdf-from-url", produces = MediaType.APPLICATION_PDF_VALUE)
-    public ResponseEntity<byte[]> generatePdfFromUrl(@RequestBody String url) throws IOException {
+    @PostMapping("/generate-pdf-from-url")
+    public ResponseEntity<byte[]> generatePdfFromUrl(
+        @RequestBody String url,
+        @RequestParam(value = "fileName", defaultValue = "download") String fileName
+    ) throws IOException {
         byte[] pdfContent = htmlToPdfService.generatePdfFromUrl(url);
-
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_PDF);
-        headers.add("Content-Disposition", "inline; filename=output.pdf");
-
+        headers.setContentDisposition(ContentDisposition.builder("attachment").filename(fileName + ".pdf").build());
         return new ResponseEntity<>(pdfContent, headers, HttpStatus.OK);
     }
 
-    @PostMapping(value = "/generate-pdf-from-html", produces = MediaType.APPLICATION_PDF_VALUE)
-    public ResponseEntity<byte[]> generatePdfFromHtml(@RequestBody String html) throws IOException {
+    @PostMapping("/generate-pdf-from-html")
+    public ResponseEntity<byte[]> generatePdfFromHtml(
+        @RequestBody String html,
+        @RequestParam(value = "fileName", defaultValue = "download") String fileName
+    ) throws IOException {
         byte[] pdfContent = htmlToPdfService.generatePdfFromHtml(html);
-
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_PDF);
-        headers.add("Content-Disposition", "inline; filename=output.pdf");
-
+        headers.setContentDisposition(ContentDisposition.builder("attachment").filename(fileName + ".pdf").build());
         return new ResponseEntity<>(pdfContent, headers, HttpStatus.OK);
     }
 }
